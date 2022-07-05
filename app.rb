575a628c0b7194fcc89578b651b2edcf7bb5e321
Adekunle Ismail <adismail4@gmail.com>
2022-07-05 12:44:17 +0100

    add main, item and app.rb

---
+++ app.rb
@@ -0,0 +1,37 @@
+class AppMethod
+
+  def create_book()
+    puts 'create book'
+  end
+
+  def create_music()
+    puts 'create music'
+  end
+
+  def create_game()
+    puts 'create game'
+  end
+
+  def list_books()
+    puts 'book list is empty'
+  end
+  def list_musics()
+    puts 'music list is empty'
+  end
+  def list_games()
+    puts 'game list is empty'
+  end
+
+  def list_authors()
+    puts 'author list is empty'
+  end
+  def list_genres()
+    puts 'genre list is empty'
+  end
+  def list_labels()
+    puts 'label list is empty'
+  end
+
+  
+  
+end
---
+++ main.rb
@@ -0,0 +1,68 @@
+require './app'
+class MainEntry
+
+  puts 'Welcome to catalog of my things App'
+
+   def self.page
+    puts "\n"
+    puts 'Please choose an option by entering a number: '
+
+      @content = {
+      '1' => 'List all books',
+      '2' => 'List all music albums', 
+      '3' => 'List of games',
+      '4' => 'List all genres (e.g \'Comedy\', \'Thriller\')',
+      '5' =>  'List all labels (e.g. \'Gift\', \'New\')',
+      '6' =>  'List all authors (e.g. \'Stephen King\')',  
+      '7' => 'Add a book',
+      '8' => 'Add a music album',
+      '9' => 'Add a game',
+      '10' => 'Exit'
+     } 
+
+
+    @content.each do |index, string|
+      puts "#{index} - #{string}"
+    end
+
+    Integer(gets.chomp)
+  end
+
+  method = AppMethod.new
+
+  loop do
+    case page
+    when 1
+      method.list_books
+    when 2
+      method.list_musics
+    when 3
+      method.list_games
+    when 4
+      method.list_authors
+    when 5
+      method.list_genres
+    when 6
+      method.list_labels
+    when 7
+      method.create_book
+    when 8
+      method.create_music
+    when 9
+      method.create_game
+    when 10
+      puts 'Thank you for using the app!'
+      exit    
+    else
+      puts 'Choose a number between 1 to 9'
+    end
+  end
+
+ 
+end
+
+def main
+  MainEntry.New
+end
+
+main
