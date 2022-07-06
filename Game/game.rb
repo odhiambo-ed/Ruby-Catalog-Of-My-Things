170ed96e79cb058bd1646080c2ad05701fe7fe90
joel <joelkalema63@gmail.com>
2022-07-06 22:18:28 +0200

    🚨 fix linters

--- Game/game.rb
+++ Game/game.rb
@@ -1,14 +1,13 @@
 require './item'
 
 class Game
-
   attr_accessor :multiplayer, :last_played_at
 
   def initialize(multiplayer, last_played_at, publish_date = Time.now.year)
-   super(publish_date)
+    super(publish_date)
 
-   @multiplayer = multiplayer
-   @last_played_at = last_played_at
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
   end
 
   def can_be_archived?()
--- app.rb
+++ app.rb
@@ -18,9 +18,11 @@ class AppMethod
   def list_books()
     puts 'book list is empty'
   end
+
   def list_musics()
     puts 'music list is empty'
   end
+
   def list_games()
     puts 'game list is empty'
   end
@@ -28,10 +30,12 @@ class AppMethod
   def list_authors()
     puts 'author list is empty'
   end
+
   def list_genres()
     puts 'genre list is empty'
   end
+
   def list_labels()
     puts 'label list is empty'
-  end 
+  end
 end
