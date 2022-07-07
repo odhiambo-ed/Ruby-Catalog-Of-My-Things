479bf72dc8b8fb301cc90e428c2a98c4635048d5
Adekunle Ismail <adismail4@gmail.com>
2022-07-07 18:10:12 +0100

    Modified app.rb

--- classes/app.rb
+++ classes/app.rb
@@ -1,10 +1,15 @@
 require_relative './music_album'
 require_relative './genre'
+require_relative '../modules/preserver_module'
 
 class App
-  def initialize
+  include PreserverModule
+
+   def initialize
     @albums = []
     @genres = []
+   
+    load_data
   end
 
   def add_album(album_name, publish_date, genre_name, on_spotify)
@@ -30,26 +35,39 @@ class App
   def list_all_albums
     puts "\nNote: No albums available." if @albums.empty?
 
-    puts "\n"
-    puts 'ALL ALBUMS'
-    puts "Genre \t\t| On spotify? \t| Album Name \t\t| Publish Date"
-
+    puts "\n----------------------------"
+    puts "\nALL ALBUMS\n\n"
+    puts "\Genre \t| On spotify? \t| Album Name \t| Publish Date"
+    puts '-------------------------------------------------------'
     @albums.each do |album|
-      puts "#{album['genre'].to_s.strip} \t\t| #{album['on_spotify?'].to_s.strip.rjust(10)} \t|
-      #{album['album_name'].to_s.strip.rjust(10)} \t\t| #{album['publish_date'].to_s.strip.rjust(10)}"
+      puts "#{album['genre'].to_s.strip} \t| #{album['on_spotify?'].to_s.strip.rjust(10)} \t| #{album['album_name'].to_s.strip.rjust(10)} \t| #{album['publish_date'].to_s.strip.rjust(10)}"
+      puts "\n---------------------------------------------------"
     end
   end
 
   def list_all_genres
     puts "\nNote: No genres available." if @genres.empty?
 
-    puts "\n"
-    puts 'ALL GENRES'
-    puts 'Name'
-    puts "\n"
+    puts "\n----------------------------"
+    puts "\nALL GENRES\n\n"
+    puts "\Name"
+    puts '----------------------------'
     @genres.each do |genre|
       puts genre['genre_name'].to_s.strip
-      puts "\n"
+      puts "\n----------------------------"
     end
+  end
+
+   def preserve_files
+    save_data_as_json(@albums, 'albums')
+    save_data_as_json(@genres, 'genres')
+  
+  end
+
+  private
+
+  def load_data
+    @albums = load_file('albums')
+    @genres = load_file('genres')    
   end
 end
